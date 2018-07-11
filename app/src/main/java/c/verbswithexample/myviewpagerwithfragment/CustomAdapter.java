1fa9a3855414a839c621472b4ac1e3001c68bc5a
a4anddev <a4anddev@gmail.com>
2018-07-11 07:47:43 +0530

    viewPager create with fragment

--- app/src/main/java/c/verbswithexample/myviewpagerwithfragment/CustomAdapter.java
+++ app/src/main/java/c/verbswithexample/myviewpagerwithfragment/CustomAdapter.java
@@ -4,8 +4,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentFive;
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentFour;
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentOne;
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentSix;
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentThree;
+import c.verbswithexample.myviewpagerwithfragment.AllFragments.FragmentTwo;
+
 public class CustomAdapter extends FragmentPagerAdapter{
 
+    private final int ITEMS = 6;
 
     public CustomAdapter(FragmentManager fm) {
         super(fm);
@@ -13,11 +21,33 @@ public class CustomAdapter extends FragmentPagerAdapter{
 
     @Override
     public Fragment getItem(int i) {
-        return null;
+
+        switch (i){
+            case 0:
+                return new FragmentOne();
+            case 1:
+                return new FragmentTwo();
+            case 2:
+                return new FragmentThree();
+            case 3:
+                return new FragmentFour();
+            case 4:
+                return new FragmentFive();
+            case 5:
+                return new FragmentSix();
+
+                default:
+                    return new FragmentOne();
+
+
+
+        }
+
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return ITEMS;
     }
 }
--- app/src/main/java/c/verbswithexample/myviewpagerwithfragment/MainActivity.java
+++ app/src/main/java/c/verbswithexample/myviewpagerwithfragment/MainActivity.java
@@ -1,5 +1,6 @@
 package c.verbswithexample.myviewpagerwithfragment;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPagerId);
+
+        CustomAdapter customAdapter = new CustomAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(customAdapter);
+
+
     }
 }
